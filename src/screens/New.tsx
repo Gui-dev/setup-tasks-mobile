89e11eb44ecaf1da2e42bc2a5dfcd1e52ecdb7dd
Gui-dev <adrianon10@gmail.com>
2023-02-06 03:32:14 -0300

    feat: add helper function handleCreateNewHabit

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
-import { ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { ActivityIndicator, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 import colors from 'tailwindcss/colors'
+import Toast from 'react-native-toast-message'
 
 import { BackButton } from '../components/BackButton'
 import { Checkbox } from '../components/Checkbox'
+import { api } from '../lib/api'
 
 export const New = () => {
   const [weekDays, setWeekDays] = useState<number[]>([])
+  const [loading, setLoading] = useState(false)
+  const [title, setTitle] = useState('')
   const avaliableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
 
   const handleToggleWeekDay = (weekDayIndex: number) => {
@@ -17,6 +21,49 @@ export const New = () => {
       setWeekDays(prevState => [...prevState, weekDayIndex])
     }
   }
+
+  const handleCreateNewHabit = async () => {
+    try {
+      setLoading(true)
+      if (!title.trim()) {
+        Toast.show({
+          type: 'info',
+          text1: 'Opsss',
+          text2: 'Informe o nome do hábito'
+        })
+        return
+      }
+      if (weekDays.length === 0) {
+        Toast.show({
+          type: 'info',
+          text1: 'Opsss',
+          text2: 'você deve selecionar um ou mais dias'
+        })
+        return
+      }
+      await api.post('/habits', {
+        title,
+        weekDays
+      })
+      setTitle('')
+      setWeekDays([])
+      Toast.show({
+        type: 'success',
+        text1: 'Parabéns',
+        text2: 'Hábito criado com sucesso'
+      })
+    } catch (error) {
+      Toast.show({
+        type: 'error',
+        text1: 'Opsss',
+        text2: 'não foi possível criar um novo hábito'
+      })
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <View className="flex-1 px-8 pt-16 bg-background">
       <BackButton />
@@ -36,6 +83,8 @@ export const New = () => {
           className="text-white mt-3 pl-4 h-12 bg-zinc-900 rounded-lg border-2 border-zinc-800 focus:border-green-600"
           placeholder="ex: Exercícios, estudar, alimentação, etc..."
           placeholderTextColor={colors.zinc[400]}
+          value={title}
+          onChangeText={setTitle}
         />
         <Text className="text-base text-white font-semibold mt-4 mb-3">
           Qual é a recorrência ?
@@ -55,11 +104,20 @@ export const New = () => {
         <TouchableOpacity
           activeOpacity={0.7}
           className="flex-row items-center justify-center mt-6 h-14 w-full bg-green-600 rounded-md"
+          onPress={handleCreateNewHabit}
         >
-          <Feather name="check" color={colors.white} size={20} />
-          <Text className="text-base font-semibold text-white ml-2">
-            Confirmar
-          </Text>
+          {
+            !loading
+              ? <>
+                <Feather name="check" color={colors.white} size={20} />
+                <Text className="text-base font-semibold text-white ml-2">
+                  Confirmar
+                </Text>
+              </>
+
+              : <ActivityIndicator color={colors.white} size="large" />
+          }
+
         </TouchableOpacity>
       </ScrollView>
     </View>
