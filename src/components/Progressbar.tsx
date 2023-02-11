fc1962b8834fb99ce68e67331182886d449c0990
Gui-dev <adrianon10@gmail.com>
2023-02-11 03:10:29 -0300

    style: add progress bar animation

--- src/components/Progressbar.tsx
+++ src/components/Progressbar.tsx
@@ -1,17 +1,28 @@
+import { useEffect } from 'react'
 import { View } from 'react-native'
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated'
 
 interface IProgressbar {
   progress?: number
 }
 
 export const Progressbar = ({ progress = 0 }: IProgressbar) => {
+  const sharedProgress = useSharedValue(progress)
+  const style = useAnimatedStyle(() => {
+    return {
+      width: `${sharedProgress.value}%`
+    }
+  })
+
+  useEffect(() => {
+    sharedProgress.value = withTiming(progress)
+  }, [sharedProgress, progress])
+
   return (
     <View className="mt-4 h-3 w-full bg-zinc-700 rounded-xl">
-      <View
+      <Animated.View
         className="h-3 bg-violet-700 rounded-xl"
-        style={{
-          width: `${progress}%`
-        }}
+        style={style}
       />
     </View>
   )
