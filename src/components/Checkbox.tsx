b0ce05aebd854ac31ece7dd502ee43d06c6a2847
Gui-dev <adrianon10@gmail.com>
2023-02-11 03:20:38 -0300

    style: add checkbox animation

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,6 +1,7 @@
 import { Text, TouchableOpacity, TouchableOpacityProps, View } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 import colors from 'tailwindcss/colors'
+import Animated, { ZoomIn, ZoomOut } from 'react-native-reanimated'
 
 interface ICheckbox extends TouchableOpacityProps {
   title: string
@@ -16,9 +17,13 @@ export const Checkbox = ({ title, checked, ...rest }: ICheckbox) => {
     >
       {
         checked
-          ? <View className="items-center justify-center h-8 w-8 bg-green-500 rounded-lg">
+          ? <Animated.View
+            className="items-center justify-center h-8 w-8 bg-green-500 rounded-lg"
+            entering={ZoomIn}
+            exiting={ZoomOut}
+          >
             <Feather name="check" color={colors.white} size={20} />
-          </View>
+          </Animated.View>
           : <View className="h-8 w-8 bg-zinc-900 rounded-lg" />
       }
       <Text className="text-base text-white font-semibold ml-3">
